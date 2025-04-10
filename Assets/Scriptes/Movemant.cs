3d8779f41f071729077b0ecf9f43f22363164eed
Богдан Завьялов <buguskin2015@gmail.com>
2025-04-10 14:53:25 +0500

    пмсы

--- Assets/Scriptes/Movemant.cs
+++ Assets/Scriptes/Movemant.cs
@@ -56,11 +56,11 @@ public class PlayerController : MonoBehaviour
             // Поворот персонажа
             if (moveInput > 0)
             {
-                transform.localScale = new Vector3(1, 1, 1);
+                transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
             }
             else if (moveInput < 0)
             {
-                transform.localScale = new Vector3(-1, 1, 1);
+                transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
             }
         }
 
